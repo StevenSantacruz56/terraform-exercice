92d59d32510a7fd4224c904e21b5ab11a0461b0c
Steven Santacruz Garcia <steven.santacruz@finkargo.com>
2024-10-07 15:33:54 -0500

    First commit

---
+++ dynamodb.tf
@@ -0,0 +1,26 @@
+resource "aws_dynamodb_table" "apps_storage" {
+  name           = "sss"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "ImageId"
+  range_key      = "LastUpdatedTime"
+
+  attribute {
+    name = "ImageId"
+    type = "S"
+  }
+
+  attribute {
+    name = "LastUpdatedTime"
+    type = "S"
+  }
+
+  tags = {
+    environment = "test"
+    project     = "billie"
+  }
+}
+
+
+
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "bucket_name" {
+  type = string
+  default = "test-billie-nala"
+  description = "The bucket name"
+}
